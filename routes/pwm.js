bd3724d5eb6434c735d9d1f048eb993fa875ef05
Alex Hamilton <alex.hamilton@ou.edu>
2017-09-01 19:14:38 -0500

    Fix defaults and such.

--- routes/pwm.js
+++ routes/pwm.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const Pin = require('../lib/pin')
 
 router.get('/pwm', (req, res, next) => {
-	let target = new Pin(req.query.pin || 7)
-	let pulseWidth = req.query.width || 1.5
-	let frequency = req.query.frequency || 1/3000
-	let duration = req.frequency.duration || 5000
+	let query = req.query || {}
+	let target = new Pin(query.pin || 7)
+	let pulseWidth = query.width || 1.5
+	let frequency = query.frequency || 2
+	let duration = query.duration || 5000
 	target.doPWM(pulseWidth, frequency, duration).then(() => {
 		res.json({
 			width: pulseWidth,
