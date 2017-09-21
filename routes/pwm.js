6a00d66e2a468734a173d0b591f3475277abf313
Alex Hamilton <alex.hamilton@ou.edu>
2017-09-21 12:02:45 -0500

    Floats and ints.

--- routes/pwm.js
+++ routes/pwm.js
@@ -3,10 +3,10 @@ const Pin = require('../lib/pin')
 
 router.get('/pwm', (req, res, next) => {
 	let query = req.query || {}
-	let target = new Pin(query.pin || 7)
-	let pulseWidth = query.width || 1.5
-	let frequency = query.frequency || 50
-	let duration = query.duration || 5000
+	let target = new Pin(query.pin ? parseInt(query.pin) : 7)
+	let pulseWidth = query.width ? parseFloat(query.width) : 1.5
+	let frequency = query.frequency ? parseInt(query.frequency) : 50
+	let duration = query.duration ? parseInt(query.duration) : 5000
 	target.doPWM(pulseWidth, frequency, duration).then(() => {
 		res.json({
 			width: pulseWidth,
