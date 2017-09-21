e88e6e38a965d10cc8fc3950db01e8e5220278ba
Alex Hamilton <alex.hamilton@ou.edu>
2017-09-21 12:45:18 -0500

    Add hardware PWM option.

--- routes/pwm.js
+++ routes/pwm.js
@@ -3,17 +3,23 @@ const Pin = require('../lib/pin')
 
 router.get('/pwm', (req, res, next) => {
 	let query = req.query || {}
+	let hard = !!query.hard
 	let target = new Pin(query.pin ? parseInt(query.pin) : 7)
 	let pulseWidth = query.width ? parseFloat(query.width) : 1.5
 	let frequency = query.frequency ? parseInt(query.frequency) : 50
 	let duration = query.duration ? parseInt(query.duration) : 5000
-	target.doPWM(pulseWidth, frequency, duration).then(() => {
-		res.json({
-			width: pulseWidth,
-			frequency: frequency,
-			duration: duration
-		})
-	}).catch(next)
+
+	doTheThing = ((req, res, pulseWidth, frequency, duration) => res.json({
+		width: pulseWidth,
+		frequency: frequency,
+		duration: duration
+	})).bind(this, req, res, pulseWidth, frequency, duration)
+
+	if(hard) {
+		target.pwm(pulseWidth, frequency, duration).then(doTheThing).catch(next)
+	} else {
+		target.doPWM(pulseWidth, frequency, duration).then(doTheThing).catch(next)
+	}
 })
 
 module.exports = router
