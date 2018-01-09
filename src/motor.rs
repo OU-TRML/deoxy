6cb47d94d034c8846863a06e886973721c430e94
Alex Hamilton <alex.hamilton@ou.edu>
2018-01-08 21:48:12 -0600

    Add some tests.

--- src/motor.rs
+++ src/motor.rs
@@ -40,19 +40,37 @@ impl Motor {
 		motor
 	}
 
+	/// The minimum usable pulse width, as specified upon creation.
+	/// # Examples
+	/// ```
+	/// use std::time::Duration;
+	/// use deoxy::Motor;
+	/// let motor = Motor::new(0, Duration::from_millis(20), (Duration::new(0, 900_000), Duration::new(0, 1_200_000)));
+	/// assert_eq!(motor.min(), Duration::new(0, 900_000));
+	/// ```
 	pub fn min(&self) -> Duration {
 		self.range.0
 	}
 
+	/// The maximum usable pulse width, as specified upon creation.
+	/// # Examples
+	/// ```
+	/// use std::time::Duration;
+	/// use deoxy::Motor;
+	/// let motor = Motor::new(0, Duration::from_millis(20), (Duration::new(0, 900_000), Duration::new(0, 1_200_000)));
+	/// assert_eq!(motor.max(), Duration::new(0, 1_200_000));
+	/// ```
 	pub fn max(&self) -> Duration {
 		self.range.1
 	}
 
+	/// Sets the pulse width to the center of the possible range, bringing it to the neutral position.
 	pub fn set_neutral(&mut self) -> Result<(), MotorError> {
 		let (min, max) = (self.min(), self.max());
 		self.set_pulse_width((min + max) / 2)
 	}
 
+	/// Attempts to set the pulse width to the given duration.
 	pub fn set_pulse_width(&mut self, pulse_width: Duration) -> Result<(), MotorError> {
 		if pulse_width < self.min() || pulse_width > self.max() {
 			Err(MotorError::OutOfBounds)
