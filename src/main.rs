61e144bda7a1cfbcf821dfff1e5518f36823c16e
Alex Hamilton <alex.hamilton@ou.edu>
2018-01-13 20:31:19 -0600

    Add config file support.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 extern crate deoxy;
 use deoxy::communication::{Hub, Message};
 use deoxy::config::Config;
+use std::env;
 
 fn main() {
-	let hub = Hub::new(3);
-	let mut queue = vec![];
-	for i in 0..3 {
+	// Config file support
+	let mut args = env::args();
+	let config = if let Some(index) = args.position(|arg| arg == "--config-path" || arg == "-c" || arg == "--c") {
+		if let Some(path) = args.nth(index + 1) {
+			Config::read_or_default(&path)
+		} else {
+			Config::default()
+		}
+	} else {
+		Config::default()
+	};
+	let order = config.order();
+	let hub = Hub::new(order as u8);
+	let mut queue = Vec::with_capacity(order);
+	for i in 0..order {
 		queue.push(hub.send(i, Message::Debug(format!("Hello, thread {}!", i))));
 	}
 	loop { }
