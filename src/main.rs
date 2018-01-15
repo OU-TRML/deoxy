9e7f8e453fbec46e784a0d95aae12776d2a6adcc
Alex Hamilton <alex.hamilton@ou.edu>
2018-01-14 18:09:01 -0600

    Add command-line flag stuff and a help message.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 extern crate deoxy;
 use deoxy::communication::{Hub, Message};
-use deoxy::config::Config;
 use std::env;
+mod flags;
+use flags::Flag;
+mod config;
+use config::Config;
+
+macro_rules! print_help {
+	($fmt:expr, $($arg:tt)*) => ({
+		println!($fmt, $($arg),*); // What the heck is this magic
+		print_help!();
+	});
+	($arg:tt) => ({
+		print_help!("{}", $arg);
+	});
+	() => ({
+		println!("usage: {name} [--help] [--config-path <path>]", name=env::args().nth(0).unwrap_or("deoxy".to_string()));
+	});
+}
 
 fn main() {
 	// Config file support
-	let mut args = env::args();
-	let config = if let Some(index) = args.position(|arg| arg == "--config-path" || arg == "-c" || arg == "--c") {
-		if let Some(path) = args.nth(index + 1) {
-			Config::read_or_default(&path)
-		} else {
-			Config::default()
+	let args = env::args();
+	match Flag::from(args) {
+		Ok(flags) => {
+			let config = flags.iter().map(|x| match x { &Flag::ConfigPath(ref path) => Some(path), _ => None }).last().map(|ref path| Config::read_or_default(path.unwrap().as_str())).unwrap_or_default();
+			let order = config.order();
+			let hub = Hub::new(order as u8);
+			let mut queue = Vec::with_capacity(order);
+			for i in 0..order {
+				queue.push(hub.send(i, Message::Debug(format!("Hello, thread {}!", i))));
+			}
+			loop { }
+		}, Err(text) => {
+			if let Some(message) = text {
+				print_help!(message);
+			} else {
+				print_help!();
+			}
 		}
-	} else {
-		Config::default()
-	};
-	let order = config.order();
-	let hub = Hub::new(order as u8);
-	let mut queue = Vec::with_capacity(order);
-	for i in 0..order {
-		queue.push(hub.send(i, Message::Debug(format!("Hello, thread {}!", i))));
 	}
-	loop { }
 }
