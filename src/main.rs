a8606087c7b9214faac52cddf1d101006f575a5d
Alex Hamilton <alex.hamilton@ou.edu>
2018-02-23 10:35:36 -0600

    Rewrite main.rs (begin the restructure).

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,21 @@ use motion::Motor;
 
 fn main() {
 	let (tx, rx) = mpsc::channel();
+	let pin = 17;
+	let (period, min, max) = (Duration::from_millis(20), Duration::new(0, 553_000), Duration::new(0, 2_520_000)); // TODO: User input
 	let child = thread::spawn(move || {
 		let pin = rx.recv().unwrap();
-		let mut motor = Motor::new(pin, Duration::from_millis(20), Duration::new(0, 553_000)..Duration::new(0, 2_520_000));
-		let total = Duration::from_millis(20);
+		let mut motor = Motor::new(pin, period, min..max); // min..=max
 		let _ = motor.set_angle(0.0).unwrap();
 		let zero = motor.get_pulse_width();
 		motor.set_neutral();
 		let neutral = motor.get_pulse_width();
 		for _ in 0..10 {
 			for _ in 0..20 {
-				motor.do_wave(neutral, total);
+				motor.do_wave(neutral, period);
 			}
 			for _ in 0..20 {
-				motor.do_wave(zero, total);
+				motor.do_wave(zero, period);
 			}
 		}
 	});
