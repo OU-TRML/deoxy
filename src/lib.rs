3ee646df1728d7401ee49c604cec0b5c3b6b35c6
Alex Hamilton <alex.hamilton@ou.edu>
2018-02-24 14:03:58 -0600

    Iterate over all given motors (i.e. use the config file for more than the first entry).
    Closes #2.

--- src/lib.rs
+++ src/lib.rs
@@ -17,17 +17,18 @@ use communication::{Slave, Action};
 use config::Config;
 
 pub fn main(config: Config) {
-	let first = config.motors.first.unwrap();
-	let pin = first.pin;
-	let (period, min, max) = (Duration::from_millis(first.period), Duration::new(0, first.min * 1000), Duration::new(0, first.max * 1000));
-	let (slave, maw) = Slave::create_with_channel(pin, period, min..max);
-	let child = thread::spawn(move || {
-		slave._loop();
-	});
-	let _ = maw.send(Action::Close).unwrap(); // TODO: Error handling
-	let _ = maw.send(Action::Open(Duration::from_millis(first.period * 100))).unwrap(); // TODO: Error handling
-	let result = child.join();
-	if let Err(err) = result {
-		println!("Error: {:?}", err);
+	for spec in config.get_motors() {
+		let pin = spec.get_pin();
+		let (period, min, max) = (Duration::from_millis(spec.get_period()), Duration::new(0, spec.get_min() * 1000), Duration::new(0, spec.get_max() * 1000));
+		let (slave, maw) = Slave::create_with_channel(pin, period, min..max);
+		let child = thread::spawn(move || {
+			slave._loop();
+		});
+		let _ = maw.send(Action::Close).unwrap(); // TODO: Error handling
+		let _ = maw.send(Action::Open(Duration::from_millis(spec.get_period() * 100))).unwrap(); // TODO: Error handling
+		/* let result = child.join();
+		if let Err(err) = result {
+			println!("Error: {:?}", err);
+		}*/
 	}
 }
