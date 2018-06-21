94bcd9b93d433bd52ea36f6e6f1406a7abf7d046
Alex Hamilton <alex.hamilton@ou.edu>
2018-06-20 21:33:40 -0400

    Add Slave stuff.

--- src/communication.rs
+++ src/communication.rs
@@ -1,11 +1,11 @@
 use config::MotorSpec;
 
 use motion::Motor;
-use std::sync::{Arc, Mutex, mpsc};
+use std::collections::VecDeque;
+use std::ops::Range;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
-use std::ops::Range;
-use std::collections::VecDeque;
 
 pub type Delay = Duration;
 
@@ -31,6 +31,23 @@ pub struct Slave {
 }
 
 impl Slave {
+	/// Creates a slave and communication (mpsc) channel for the given motor specs.
+    pub fn create_with_channel(
+        pin_number: u16,
+        period: Duration,
+        signal_range: Range<Duration>,
+    ) -> (Self, mpsc::Sender<Action>) {
+        let (tx, rx) = mpsc::channel();
+        let motor = Arc::new(Mutex::new(Motor::new(pin_number, period, signal_range)));
+        (
+            Self {
+                rx,
+                motor,
+                queue: VecDeque::new(),
+            },
+            tx,
+        )
+    }
 
 	pub fn create_with_channel(pin_number: u16, period: Duration, signal_range: Range<Duration>) -> (Self, mpsc::Sender<Action>) {
 		let (tx, rx) = mpsc::channel();
@@ -88,6 +105,32 @@ impl Slave {
 		}
 	}
 
+    /// The entire *raison d'être* for `Slave` instances.
+    /// This method causes both the motor and the handler to loop.
+    /// # Notes
+    /// You **must** call this method for this struct to be useful at all.
+    pub fn _loop(mut self) {
+        // TODO: Handle timing as well (instead of performing everything on the next tick)
+        let motor = Arc::clone(&self.motor);
+        let _child = thread::spawn(move || loop {
+            let mut motor = motor.lock().unwrap();
+            motor.do_wave();
+        });
+        loop {
+            match self.rx.try_recv() {
+                Ok(action) => self.handle(action),
+                _ => {} // TODO: Handle error
+            }
+            if let Some(action) = self.queue.pop_front() {
+                let now = Instant::now();
+                if now >= action.0 {
+                    self.handle(action.1);
+                } else {
+                    self.queue.push_front(action);
+                }
+            }
+        }
+    }
 }
 
 pub struct Coordinator {
@@ -96,29 +139,10 @@ pub struct Coordinator {
 
 impl From<Vec<MotorSpec>> for Coordinator {
 
-	///
-	///
-	/// #Notes
-	/// This method will spin up child threads and start `Slave`s looping (which is why it moves its argument).
-	/// Once it has been used, the motors *will* be receiving messages immediately.
-	///
-	/// The message sent to the motor is simply `Action::Close`, so that the motor immediately goes to the closed position if it is not already.
-	/// #Panics
-	/// This method will `panic!` if sending the an `Action::Close` to the child thread fails.
-	fn from(motors: Vec<MotorSpec>) -> Self {
-		Self {
-			channels: motors.iter().map(|spec| {
-				let pin = spec.get_pin();
-				let (period, min, max) = (Duration::from_millis(spec.get_period()), Duration::new(0, spec.get_min() * 1000), Duration::new(0, spec.get_max() * 1000));
-				Slave::create_with_channel(pin, period, min..max)
-			}).map(move |(slave, maw)| {
-				let _child = thread::spawn(move || {
-					slave._loop();
-				});
-				let _ = maw.send(Action::Close).unwrap(); // TODO: Error handling
-				maw
-			}).collect::<Vec<_>>()
+impl Drop for Coordinator {
+	fn drop(&mut self) {
+		while let Some(channel) = self.channels.pop() {
+			channel.send(Action::Close).unwrap();
 		}
 	}
-
 }
