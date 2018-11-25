3b9d19d30b288f81fae765fd109da9999ccbf723
Alex Hamilton <alex.hamilton@ou.edu>
2018-11-25 17:43:01 -0600

    Update to latest rustfmt style.

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ extern crate deoxy;
 
 fn main() {
     let matches = clap_app!(deoxy =>
-		(version: "0.2.0")
-		(author: "Alex Hamilton <alex.hamilton@ou.edu>")
-		(about: "For all your buffer exchange needs!")
-		(setting: settings::SubcommandRequired)
-		(@arg CONFIG: -c --config +takes_value "Sets a custom config file")
-		(@subcommand run =>
-			// (about: "starts the deoxy daemon, or aborts if another instance is detected")
-			(about: "runs deoxy::main()")
-			)
-		)
+    (version: "0.2.0")
+    (author: "Alex Hamilton <alex.hamilton@ou.edu>")
+    (about: "For all your buffer exchange needs!")
+    (setting: settings::SubcommandRequired)
+    (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
+    (@subcommand run =>
+        // (about: "starts the deoxy daemon, or aborts if another instance is detected")
+        (about: "runs deoxy::main()")
+        )
+    )
     .get_matches();
 
     let config = matches
